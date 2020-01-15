89eb57ff20b041ccb73159c21db323bb9be7f4a6
petrovakd <petrovakd@lad24.ru>
2020-01-15 11:03:17 +0300

    change qrcode version

--- example/App.js
+++ example/App.js
@@ -1,33 +1,92 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react'
+import { StyleSheet, ScrollView, View, Button } from 'react-native'
+import QRCode from 'react-native-qrcode-svg'
 
-import React from 'react'
-import {
-  StyleSheet,
-  View
-} from 'react-native'
+const PATH_TO_LOGO = './logo.jpg'
 
-import QR from 'react-native-qrcode-svg'
-
-const App: () => React$Node = () => {
-  return (
-    <View style={styles.container}>
-      <QR />
-    </View>
-  )
+type Props = {}
+export default class App extends Component<Props> {
+  handlePress = () => {
+    this.qrcode.toDataURL(this.callback)
+  }
+  callback = (dataURL) => {
+    console.log(dataURL)
+  }
+  render () {
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.section}>
+          <QRCode value='hello world' />
+        </View>
+        <View style={styles.section}>
+          <QRCode
+            value='hello world'
+            size={200}
+          />
+        </View>
+        <View style={styles.section}>
+          <QRCode
+            value='hello world'
+            color='blue'
+            backgroundColor='yellow'
+          />
+        </View>
+        <View style={styles.section}>
+          <QRCode
+            value='hello world'
+            logo={require(PATH_TO_LOGO)}
+          />
+        </View>
+        <View style={styles.section}>
+          <QRCode
+            value='hello world'
+            logo={require(PATH_TO_LOGO)}
+            logoSize={50}
+          />
+        </View>
+        <View style={styles.section}>
+          <QRCode
+            value='hello world'
+            logo={require(PATH_TO_LOGO)}
+            logoMargin={10}
+          />
+        </View>
+        <View style={styles.section}>
+          <QRCode
+            value='hello world'
+            logo={require(PATH_TO_LOGO)}
+            logoBorderRadius={15}
+          />
+        </View>
+        <View style={styles.section}>
+          <QRCode
+            value='hello world'
+            logo={require(PATH_TO_LOGO)}
+            logoBackgroundColor='blue'
+            getRef={(c) => (this.qrcode = c)}
+          />
+          <Button title={'getDataURL'} onPress={this.handlePress} />
+        </View>
+        <View style={styles.section}>
+          <QRCode
+            value='hello world'
+            ecl='H'
+          />
+        </View>
+      </ScrollView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#F5FCFF',
+    paddingTop: 15,
+    paddingBottom: 15
+  },
+  section: {
+    marginTop: 15,
+    marginBottom: 15
   }
 })
-
-export default App
